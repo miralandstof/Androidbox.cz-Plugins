84773a8cf57cbe7a743a94fef4002b5fc5c7f588
miralandstof <miralandstof@androidbox.cz>
2026-09-16 07:36:24 +0200

    Add Site Manager plugin source

---
+++ androidbox-site-manager/androidbox-site-manager.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: AndroidBox.cz Site Manager
+ * Description: Administration and content management tools for AndroidBox.cz.
+ * Version: 4.2.0
+ * Author: AndroidBox.cz
+ * Update URI: https://github.com/miralandstof/Androidbox.cz-Plugins/
+ */
+
